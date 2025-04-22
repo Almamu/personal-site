6370643dc5eb31059b7446423e7b6764bcf2af63
Alexis Maiquez Murcia <personal-site@alma.mu>
2025-04-22 17:36:04 +0200

    feat: use ssg

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import "./index.css";
 
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { ViteReactSSG } from "vite-react-ssg/single-page";
 
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
+export const createRoot = ViteReactSSG(
   <StrictMode>
     <App />
   </StrictMode>,
